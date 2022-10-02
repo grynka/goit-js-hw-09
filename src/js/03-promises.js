7f782fe4d0f1a2caf719aee482ecf5dabb5ede2b
grynka <ihor.kozhemyakin@gmail.com>
2022-10-03 00:10:32 +0300

    ready part 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import Notiflix from 'notiflix';
+const first = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+const send = document.querySelector('.form');
+let i;
+
+
+send.addEventListener('submit', function (event) {
+  event.preventDefault()
+  i = amount.value
+  timerId = setInterval(() => {
+    if (i > 0) {
+      i -= 1
+      console.log(i+1);
+      createPromise(i+1, first.value);
+    }
+    return
+    }, step.value);
+});
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`));
+      } else {
+        reject(Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+      }
+    }, delay);
+    
+});
 }

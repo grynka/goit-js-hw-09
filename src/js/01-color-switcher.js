4b7c890acbf45e8003cc8547cfdbf6e33b287b76
grynka <ihor.kozhemyakin@gmail.com>
2022-10-03 20:18:23 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,22 +3,25 @@ const STOP = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 let timerId;
 
-stop.disabled = true;
+STOP.disabled = true;
 
 START.addEventListener('click', start);
 STOP.addEventListener('click', stop);
 
 function start() {
+    START.disabled = true;
+    STOP.disabled = false;
     timerId = setInterval(() => {
       body.style.backgroundColor = getRandomHexColor();
       }, 1000);
-      start.disabled = true;
-    stop.disabled = false;
+
     console.log(timerId);
-    return timerId;
+    //return timerId;
 }
 
 function stop() {
+    START.disabled = false;
+    STOP.disabled = true;
     console.log(timerId);
    const timer = timerId
     clearInterval(timer);
